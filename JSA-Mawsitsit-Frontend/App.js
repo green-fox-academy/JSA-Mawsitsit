a5af88d09c1df8c69dd25e2513ba75c491b6be82
Earvin Li <earvin.tli@gmail.com>
2019-11-13 18:27:16 +0800

    fix(JSAM-9): Fix little eslint errors

--- JSA-Mawsitsit-Frontend/App.js
+++ JSA-Mawsitsit-Frontend/App.js
@@ -1,19 +1,26 @@
+// External Dependencies
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>JSA-Mawsitsit frontend</Text>
-    </View>
-  );
-}
-
+// Local Variables
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
 });
+
+// Component Definition
+const App = () => (
+  <View style={styles.container}>
+    <Text>JSA Mawsitsit Frontend</Text>
+  </View>
+);
+
+export default App;
