25847eb54910c6ab5d87085ac3d99c79f8521a52
Binjie <im.binjie.zhang@outlook.com>
2019-11-13 16:34:19 +0800

    docs(JSAM-14):README is updated with instructions

--- JSA-Mawsitsit-Frontend/App.js
+++ JSA-Mawsitsit-Frontend/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TEXT } from 'react-native-dotenv';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{TEXT}</Text>
+      <Text>JSA-Mawsitsit frontend</Text>
     </View>
   );
 }
