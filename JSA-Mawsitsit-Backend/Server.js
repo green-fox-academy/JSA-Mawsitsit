5866798a08d5a4846f1e25800372313f5d2d5f8e
Cola LIAN <Suran_Lian@epam.com>
2019-11-13 15:31:57 +0800

    fix(JSAM-13): implement express environment

--- JSA-Mawsitsit-Backend/Server.js
+++ JSA-Mawsitsit-Backend/Server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
-const cors = require('cors');
 const express = require('express');
 const server = express();
 const PORT = (process.env.PORT);
 
 server.use(express.json());
-server.use(cors());
 
-
-server.listen(PORT);
+server.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+})
