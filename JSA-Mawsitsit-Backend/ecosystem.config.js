de39d14a32e22e008ce156d160c0cdb63916a345
Earvin Li <earvin.tli@gmail.com>
2019-11-19 17:53:22 +0800

    build(JSAM-16): add ecosystem.config.js for AWS deploying

---
+++ JSA-Mawsitsit-Backend/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'jsa-mawsitsit-backend',
+    script: './index.js',
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-199-17-156.ap-northeast-1.compute.amazonaws.com',
+      key: '~/.ssh/jsa-mawsitsit-kp.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:green-fox-academy/JSA-Mawsitsit.git',
+      path: '/home/ubuntu/jsa-mawsitsit-backend',
+      'post-deploy': 'cd JSA-Mawsitsit-Backend && npm install && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+};
