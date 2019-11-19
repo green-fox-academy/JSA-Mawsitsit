a1791d1ea55de348edd286959aa05a34cabdd243
Cola LIAN <Suran_Lian@epam.com>
2019-11-19 14:51:06 +0800

    fix(JSAM-4): Register mock backend

--- JSA-Mawsitsit-Backend/routes/registerRouter.js
+++ JSA-Mawsitsit-Backend/routes/registerRouter.js
@@ -1,36 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-let errorMessage = '';
-const weakRegex = new RegExp('^((?=.[a-z])|(?=.[0-9])|(?=.*[A-Z]))(?=.{8,})');
-const validateEmail = (email) => {
-    if(!email || (email.indexOf('@') == -1)){
-      if(errorMessage.indexOf('Error in email. ') == -1){
-        errorMessage += 'Error in email. '
-      }
-      return false
-    }
-    return true
-}
-const validatePhoneNumber = (phoneNumber) => {
-    if(!phoneNumber || (phoneNumber.length < 8)){
-      if(errorMessage.indexOf('Error in phone number. ') == -1){
-        errorMessage += 'Error in phone number. '
-      }
-      return false
-    }
-    return true
-}
-const validatePassword= (password) => {
-    if(!password || (!weakRegex.test(password))){
-      if(errorMessage.indexOf('Error in password. ') == -1){
-        errorMessage += 'Error in password. '
-      }
-      return false
-    }
-    
-    return true
-}
+const { validateEmail } = require('../helper/validation');
+const { validatePhoneNumber } = require('../helper/validation');
+const { validatePassword } = require('../helper/validation');
+
 
 router.post('/', (req, res) => {
   const { email, phone_number: phoneNumber, password } = req.body;
@@ -42,13 +16,23 @@ router.post('/', (req, res) => {
     return;
   }
 
-  validateEmail(email);
-  validatePhoneNumber(phoneNumber);
-  validatePassword(password);
+  let errorMessage = '';
+  if(validateEmail(email)){
+    errorMessage += 'Error in email. ';
+  }
+  if(validatePhoneNumber(phoneNumber)){
+    errorMessage += 'Error in phone number. ';
+  }
+  if(validatePassword(password)){
+    errorMessage += 'Error in phone password. ';
+  }
+  
+  
   if(errorMessage.length > 0){ 
     res.status(400).json({
       errorMessage,
-    });  
+    }); 
+
     return;
   }
 
