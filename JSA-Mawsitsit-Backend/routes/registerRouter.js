2d0a1115550cd463083597415c1248a912bcdc8b
Binjie <im.binjie.zhang@outlook.com>
2019-11-26 16:40:18 +0800

    fix(JSAM-35):handle error

--- JSA-Mawsitsit-Backend/routes/registerRouter.js
+++ JSA-Mawsitsit-Backend/routes/registerRouter.js
@@ -20,24 +20,23 @@ registerRouter.post('/', async (req, res) => {
     });
     return;
   }
-
-  const identifierExists = await checkIdentifier(req.body);
-  if (identifierExists) {
-    res.status(409).json({
-      message: 'Email or phone number already exist.',
-    });
-  } else {
-    try {
+  try {
+    const identifierExists = await checkIdentifier(req.body);
+    if (identifierExists) {
+      res.status(409).json({
+        message: 'Email or phone number already exist.',
+      });
+    } else {
       const userId = await registerQuery(req.body);
       res.status(200).json({
         user_id: userId,
         email,
       });
-    } catch (error) {
-      res.status(500).json({
-        message: 'Something went wrong, please try again later.',
-      });
     }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Something went wrong, please try again later.',
+    });
   }
 });
 
--- JSA-Mawsitsit-Backend/services/dataService.js
+++ JSA-Mawsitsit-Backend/services/dataService.js
@@ -1,4 +1,3 @@
-// Internal Dependencies
 const mysqlConnection = require('../database');
 
 const mysqlPromisedQuery = (dataConnection, queryStatement, queryInput = []) => new Promise(
@@ -13,24 +12,18 @@ const registerQuery = async (user) => {
   const { email, phone_number: phoneNumber, password } = user;
   const registerDetail = [email, phoneNumber, password];
   const sqlInsert = 'INSERT INTO users (email, phone_number, password) VALUES (?, ?, ?);';
-  try {
-    const response = await mysqlPromisedQuery(mysqlConnection, sqlInsert, registerDetail);
-    return response.insertId;
-  } catch (error) {
-    return error;
-  }
+  const response = await mysqlPromisedQuery(mysqlConnection, sqlInsert, registerDetail)
+    .catch((error) => { throw error; });
+  return response.insertId;
 };
 
 const checkIdentifier = async (user) => {
   const { email, phone_number: phoneNumber } = user;
   const userIdentifier = [email, phoneNumber];
   const sqlSelectByInput = 'SELECT * FROM users  WHERE email = ? AND phone_number = ?;';
-  try {
-    const response = await mysqlPromisedQuery(mysqlConnection, sqlSelectByInput, userIdentifier);
-    return response.length !== 0;
-  } catch (error) {
-    return error;
-  }
+  const response = await mysqlPromisedQuery(mysqlConnection, sqlSelectByInput, userIdentifier)
+    .catch((error) => { throw error; });
+  return response.length !== 0;
 };
 
 module.exports = { registerQuery, checkIdentifier };
