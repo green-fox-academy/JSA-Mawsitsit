4fb62d07c3a47fac9b20e7416c6acaaef838f14d
Binjie <im.binjie.zhang@outlook.com>
2019-11-28 14:33:35 +0800

    feat(JSAM-59):password encryption is add in and register backend

---
+++ JSA-Mawsitsit-Backend/migrations/1574644820304_insert_data_user.js
@@ -0,0 +1,10 @@
+module.exports = {
+  up: `
+    INSERT INTO user (email, phone_number, password) values
+    ('email1@gmail.com', '12345676', '$2a$10$XQnzHFZcA7D9w87KOxkVNeH7j9UMSjsBlC3qoUGKN0lJ7oAjW6Xl2'),
+    ('email2@gmail.com', '12345677', '$2a$10$XQnzHFZcA7D9w87KOxkVNeTZy9k72cnOFarzDXREh.eQfBBNJgDdW'),
+    ('email3@gmail.com', '12345678', '$2a$10$XQnzHFZcA7D9w87KOxkVNeUrQP64dSRU4f.H.NUHBxqJaB9bsVYna'),
+    ('email4@gmail.com', '12345679', '$2a$10$XQnzHFZcA7D9w87KOxkVNe1Ui0Z1b.o/.viG4MywNJ3XG9YNIDBc.');
+  `,
+  down: 'TRUNCATE user;',
+};
