c3a69122b99191a6f54d2fa7f1e8b8f3c2dff062
Earvin Li <earvin.tli@gmail.com>
2019-11-28 12:20:43 +0800

    feat(JSAM-58): optimize 'authMiddleware' logic

--- JSA-Mawsitsit-Backend/middleware/auth.js
+++ JSA-Mawsitsit-Backend/middleware/auth.js
@@ -3,17 +3,19 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    if (!req.body.userId || req.body.userId !== decodedToken.userId) {
-      res.status(401).json({ error: 'Please provide a valid user id.' });
+    if (!req.headers.authorization || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+      return res.status(400).json({ error: 'It\'s a bad request. Please try again.' });
     }
+
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+
     next();
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
       return res.status(500).json({ error: error.toString().split(': ')[1] });
     }
-    res.status(401).json({ error: 'It\'s a Bad request. Please try again.' });
+    res.status(401).json({ error: 'Unauthorized request. Please try again.' });
   }
   return true;
 };
