4417e525dd9cb55c6018c49f6d94e6a46699bbee
Binjie <im.binjie.zhang@outlook.com>
2019-12-03 10:52:16 +0800

    feat(JSAM-60):add redux store

--- JSA-Mawsitsit-Frontend/App.js
+++ JSA-Mawsitsit-Frontend/App.js
@@ -1,12 +1,20 @@
 // External Dependencies
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './src/rootReducer';
 
 // Internal Dependencies
 import AppNavigation from './src/navigation/AppNavigation';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 // Component Definition
 const App = () => (
-  <AppNavigation />
+  <Provider store={store}>
+    <AppNavigation />
+  </Provider>
 );
 
 export default App;
---
+++ JSA-Mawsitsit-Frontend/src/rootReducer.js
@@ -0,0 +1,4 @@
+const initState = {};
+// eslint-disable-next-line no-unused-vars
+const rootReducer = (state = initState, action) => { };
+export default rootReducer;
