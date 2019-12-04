603e116cfe753af3f8893610052e7d51da92ca81
Cola-LIAN <55380397+Cola-LIAN@users.noreply.github.com>
2019-12-04 11:01:35 +0800

    build(JSAM-20): Personal details backend: endpoint & test (#26)
    * build(JSAM-20): Personal details backend: endpoint & test
    
    * fix(JSAM-20): create mock db for jest and import middleware at server.js
    
    * fix(JSAM-20): fill with try&catch construction

--- JSA-Mawsitsit-Backend/server.js
+++ JSA-Mawsitsit-Backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const registerRouter = require('./routes/registerRouter');
 const loginRouter = require('./routes/loginRouter');
+const personalDetailsRouter = require('./routes/personalDetailsRouter');
+const authMiddleware = require('./middleware/auth');
 
 const server = express();
+
 
 server.use(express.json());
 server.use('/register', registerRouter);
 server.use('/login', loginRouter);
+server.use('/personalDetails', authMiddleware, personalDetailsRouter);
 
 module.exports = server;
