6b2ebfedb15d117407dee73b1b0831e98b8ec8ff
Cola LIAN <Suran_Lian@epam.com>
2019-12-06 15:57:25 +0800

    fix(JSAM-6):after delete owner email

--- JSA-Mawsitsit-Backend/migrations/1574852771645_insert_data_user_detail.js
+++ JSA-Mawsitsit-Backend/migrations/1574852771645_insert_data_user_detail.js
@@ -8,8 +8,7 @@ module.exports = {
     (1, 'email1@gmail.com', '12345676'),
     (2, 'email2@gmail.com', '12345677'),
     (3, 'email3@gmail.com', '12345678'),
-    (4, 'email4@gmail.com', '12345679'),
-    (5, '381495212@qq.com', '13648671121');
+    (4, 'email4@gmail.com', '12345679');
   `,
   down: 'TRUNCATE user_detail;',
 };
