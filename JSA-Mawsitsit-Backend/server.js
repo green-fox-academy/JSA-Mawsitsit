593a4da2a0ce2d63a65f2f4563b2587001ef05f0
Cola-LIAN <55380397+Cola-LIAN@users.noreply.github.com>
2019-12-06 16:01:46 +0800

    Cola email verification (#29)
    * build(JSAM-65): email verification

--- JSA-Mawsitsit-Backend/server.js
+++ JSA-Mawsitsit-Backend/server.js
@@ -1,9 +1,15 @@
+// External Dependencies
 const express = require('express');
+
+// Internal Dependencies
 const registerRouter = require('./routes/registerRouter');
 const loginRouter = require('./routes/loginRouter');
 const personalDetailsRouter = require('./routes/personalDetailsRouter');
 const authMiddleware = require('./middleware/auth');
+const { sendEmailRouter } = require('./routes/emailVerificationRouter');
+const { verificationLinkRouter } = require('./routes/emailVerificationRouter');
 
+// Local Variables
 const server = express();
 
 
@@ -11,5 +17,7 @@ server.use(express.json());
 server.use('/register', registerRouter);
 server.use('/login', loginRouter);
 server.use('/personalDetails', authMiddleware, personalDetailsRouter);
+server.use('/sendEmail', authMiddleware, sendEmailRouter);
+server.use('/verificationLink', verificationLinkRouter);
 
 module.exports = server;
--- JSA-Mawsitsit-Frontend/src/components/Menu/Menu.js
+++ JSA-Mawsitsit-Frontend/src/components/Menu/Menu.js
@@ -35,8 +35,8 @@ const Menu = () => {
         <View style={menuHeadInsideStyle}>
           <Thumbnail source={{ uri: menuHeaderImage }} />
           <View>
-            <Text style={menuHeadTextStyle}>JSA-Mawsitsit</Text>
-            <Text style={menuHeadTextStyle} note>contact@epam.com</Text>
+            <Text style={menuHeadTextStyle}>JSA Mawsitsit</Text>
+            <Text style={menuHeadTextStyle} note>contact@jpay.com</Text>
           </View>
         </View>
       </View>
