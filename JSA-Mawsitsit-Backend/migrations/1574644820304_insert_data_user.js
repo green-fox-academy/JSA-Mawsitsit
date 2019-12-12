9beafb423c990fcb4dac69369bbe3ace1475842a
Cola LIAN <Suran_Lian@epam.com>
2019-12-12 17:04:00 +0800

    connect email verification between frontend and backend

--- JSA-Mawsitsit-Backend/migrations/1574644820304_insert_data_user.js
+++ JSA-Mawsitsit-Backend/migrations/1574644820304_insert_data_user.js
@@ -4,7 +4,8 @@ module.exports = {
     ('email1@gmail.com', '12345676', '$2a$10$XQnzHFZcA7D9w87KOxkVNeH7j9UMSjsBlC3qoUGKN0lJ7oAjW6Xl2'),
     ('email2@gmail.com', '12345677', '$2a$10$XQnzHFZcA7D9w87KOxkVNeTZy9k72cnOFarzDXREh.eQfBBNJgDdW'),
     ('email3@gmail.com', '12345678', '$2a$10$XQnzHFZcA7D9w87KOxkVNeUrQP64dSRU4f.H.NUHBxqJaB9bsVYna'),
-    ('email4@gmail.com', '12345679', '$2a$10$XQnzHFZcA7D9w87KOxkVNe1Ui0Z1b.o/.viG4MywNJ3XG9YNIDBc.');
+    ('email4@gmail.com', '12345679', '$2a$10$XQnzHFZcA7D9w87KOxkVNe1Ui0Z1b.o/.viG4MywNJ3XG9YNIDBc.'),
+    ('381495212@qq.com', '12345676', '$2a$10$XQnzHFZcA7D9w87KOxkVNeH7j9UMSjsBlC3qoUGKN0lJ7oAjW6Xl2');
   `,
   down: 'TRUNCATE user;',
 };
